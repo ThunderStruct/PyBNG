025d2dd31a577f7b227da6e615e762e3c150f92c
Mohamed Shahawy <mohamedshahawy@aucegypt.edu>
2020-02-03 12:26:49 +0000

    pypi and travis-ci fix

--- build/lib/PyBNG/__init__.py
+++ build/lib/PyBNG/__init__.py
@@ -3,3 +3,6 @@
     Created on 1 Feb 2020
     @author: thunderstruct
 '''
+
+from .PyBNG import PyBNG
+from .PyBNGError import PyBNGError
--- build/lib/PyBNG/example.py
+++ build/lib/PyBNG/example.py
@@ -1,7 +1,9 @@
-from PyBNG import PyBNG
 
-bng = PyBNG(easting=519080, northing=185050)
 
-print(bng.get_latlon())
+if __name__ == '__main__':
+	from PyBNG import PyBNG
+	bng = PyBNG(easting=519080, northing=185050)
 
-print(PyBNG(lat=51.55178424773851, lon= -0.2835125528796557).get_bng())
+	print(bng.get_latlon())
+
+	print(PyBNG(lat=51.55178424773851, lon= -0.2835125528796557).get_bng())
